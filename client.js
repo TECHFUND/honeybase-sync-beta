a57661713044f411120c8be7a4893379fca0b561
@sgtn <shogo1104@gmail.com>
2015-06-18 17:15:47 +0900

    something wrong

--- client.js
+++ client.js
@@ -1,28 +1,30 @@
-(function(global){
-  var interval = 2000;
-  var initSocket = function(){
-    var sockJS = new SockJS('http://0.0.0.0:9999/chat');
-    sockJS.onopen    = function()  {
-      console.log('opened');
-    };
-    sockJS.onclose   = function()  {
-      console.log('closed');
-      interval+=100;
-      setTimeout(initSocket, interval);
-    };
-    return sockJS;
-  };
-  global.initSocket = initSocket;
-}(window));
-
 (function(){
-  var sockJS = initSocket();
+  var sockjs_url = 'http:0.0.0.0:9999/chat';
+  var sockjs = new SockJS(sockjs_url);
+  var multiplexer = new WebSocketMultiplex(sockjs);
 
-  sockJS.onmessage = function(e) {
+  var io1  = multiplexer.channel('io1');
+  io1.onmessage = function(e) {
     console.log(JSON.parse(e.data));
   };
 
-  document.getElementsByTagName("button")[0].addEventListener("click", function(e){
-    sockJS.send(JSON.stringify({content:"hoge"}));
+  var io2  = multiplexer.channel('io2');
+  io2.onmessage = function(e) {
+    console.log(JSON.parse(e.data));
+  };
+
+  var io3  = multiplexer.channel('io3');
+  io3.onmessage = function(e) {
+    console.log(JSON.parse(e.data));
+  };
+
+  document.getElementById("b1").addEventListener("click", function(e){
+    io1.send(JSON.stringify({content:"hoge1"}));
+  });
+  document.getElementById("b2").addEventListener("click", function(e){
+    io2.send(JSON.stringify({content:"hoge2"}));
+  });
+  document.getElementById("b3").addEventListener("click", function(e){
+    io3.send(JSON.stringify({content:"hoge3"}));
   });
 }());
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var sockjs = require('sockjs'),
     http = require('http'),
     redis = require('redis').createClient(6379, 'localhost'),
-    redisOther = require('redis').createClient();
+    redisOther1 = require('redis').createClient(),
+    redisOther2 = require('redis').createClient(),
+    redisOther3 = require('redis').createClient(),
+    websocket_multiplex = require('websocket-multiplex');
 
-var sockServer = sockjs.createServer();
+var service = sockjs.createServer();
 var connections = [];
+var multiplexer = new websocket_multiplex.MultiplexServer(service);
 
-sockServer.on('connection', function(conn) {
+
+var io1 = multiplexer.registerChannel('io1');
+io1.on('connection', function(conn) {
   connections.push(conn);
-
-  conn.on('data', function(message) {
-    redisOther.publish('chat-message', message);
-  });
-
   conn.on('close', function() {
     for(var i in connections) {
       if(connections[i] == conn) {
@@ -22,13 +23,52 @@ sockServer.on('connection', function(conn) {
       }
     }
   });
-
+  conn.on('data', function(message) {
+    redisOther1.publish('chat-message1', message);
+  });
 });
 
+var io2 = multiplexer.registerChannel('io2');
+io2.on('connection', function(conn) {
+  connections.push(conn);
+  conn.on('close', function() {
+    for(var i in connections) {
+      if(connections[i] == conn) {
+        connections.splice(i,1);
+        console.log('disconnected!');
+        break;
+      }
+    }
+  });
+  conn.on('data', function(message) {
+    redisOther2.publish('chat-message2', message);
+  });
+});
+
+var io3 = multiplexer.registerChannel('io3');
+io3.on('connection', function(conn) {
+  connections.push(conn);
+  conn.on('close', function() {
+    for(var i in connections) {
+      if(connections[i] == conn) {
+        connections.splice(i,1);
+        console.log('disconnected!');
+        break;
+      }
+    }
+  });
+  conn.on('data', function(message) {
+    redisOther3.publish('chat-message3', message);
+  });
+});
+
+
 var httpServer = http.createServer();
-sockServer.installHandlers(httpServer, {prefix:'/chat'});
+service.installHandlers(httpServer, {prefix:'/chat'});
 httpServer.listen(9999, '0.0.0.0'); // http://0.0.0.0:9999/chatがwebsocketハンドシェイクのエンドポイント
-redis.subscribe('chat-message');
+redis.subscribe('chat-message1');
+redis.subscribe('chat-message2');
+redis.subscribe('chat-message3');
 
 // when data
 redis.on('message', function(channel, rawMsgData) {
