0fb50750b47e72406291dbaa2ccd7aab23f16f13
@sgtn <shogo1104@gmail.com>
2015-07-24 16:35:21 +0900

    arranged

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,9 @@ def default():
   sudo("yum install git redis-server nodejs npm --enablerepo=epel -y");
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
   sudo("yum --enablerepo=remi install redis -y")
-  put("./redis.conf /etc/redis.hb.conf", use_sudo=True)
-  sudo("redis-server /etc/redis.conf &")
+  put("./conf/redis.conf /etc/redis.hb.conf", use_sudo=True)
+  sudo("redis-server /etc/redis.hb.conf")
   sudo("git clone https://github.com/TECHFUND/honeybase-sync-beta.git", warn_only=True);
   with cd("./honeybase-sync-beta"):
     sudo("npm i")
-    sudo("npm i -g forever")
-    sudo("node honeybase-sync.js")
+    sudo("node honeybase-sync.js &")
--- honeybase-sync.js
+++ honeybase-sync.js
@@ -1,11 +1,11 @@
 var forever = require('forever');
 
-var proxy = forever.start(['node', 'proxy.js'], {
+var proxy = forever.start(['node', ',/lib/proxy.js'], {
   max : 10000,
   silent : true
 });
 
-var server = forever.start(['node', 'server.js'], {
+var server = forever.start(['node', './lib/server.js'], {
   max : 10000,
   silent : true
 });
