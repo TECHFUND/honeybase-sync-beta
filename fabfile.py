4d26deb7572c79a9f38d263644e8534718511ff2
@sgtn <shogo1104@gmail.com>
2015-07-24 16:38:56 +0900

    add full of warn onky

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,12 @@ from fabric.contrib.console import confirm
 
 
 def default():
-  sudo("yum install git redis-server nodejs npm --enablerepo=epel -y");
+  sudo("yum install git redis-server nodejs npm --enablerepo=epel -y", warn_only=True);
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
-  sudo("yum --enablerepo=remi install redis -y")
+  sudo("yum --enablerepo=remi install redis -y", warn_only=True)
   put("./conf/redis.conf /etc/redis.hb.conf", use_sudo=True)
-  sudo("redis-server /etc/redis.hb.conf")
+  sudo("redis-server /etc/redis.hb.conf", warn_only=True)
   sudo("git clone https://github.com/TECHFUND/honeybase-sync-beta.git", warn_only=True);
   with cd("./honeybase-sync-beta"):
-    sudo("npm i")
-    sudo("node honeybase-sync.js &")
+    sudo("npm i", warn_only=True)
+    sudo("node honeybase-sync.js &", warn_only=True)
