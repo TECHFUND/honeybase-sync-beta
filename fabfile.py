7d7434abb664b0e0bc897d1bd913036262077675
@sgtn <shogo1104@gmail.com>
2015-07-24 16:43:13 +0900

    fixed

--- fabfile.py
+++ fabfile.py
@@ -4,12 +4,21 @@ from fabric.contrib.console import confirm
 
 
 def default():
-  sudo("yum install git redis-server nodejs npm --enablerepo=epel -y", warn_only=True);
+  install()
+  setup()
+  activate()
+
+def install():
+  sudo("yum install git redis-server nodejs npm --enablerepo=epel -y", warn_only=True)
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
   sudo("yum --enablerepo=remi install redis -y", warn_only=True)
-  put("./conf/redis.conf /etc/redis.hb.conf", use_sudo=True)
-  sudo("redis-server /etc/redis.hb.conf", warn_only=True)
+
+def setup():
+  put("./conf/redis.conf", "/etc/redis.hb.conf", use_sudo=True)
   sudo("git clone https://github.com/TECHFUND/honeybase-sync-beta.git", warn_only=True);
+
+def activate():
   with cd("./honeybase-sync-beta"):
+    sudo("redis-server /etc/redis.hb.conf", warn_only=True)
     sudo("npm i", warn_only=True)
     sudo("node honeybase-sync.js &", warn_only=True)
