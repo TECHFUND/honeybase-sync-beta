92029dc5014ab15f338964cc70bf5bd6ae8fa099
@sgtn <shogo1104@gmail.com>
2015-07-26 07:32:00 +0900

    fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,4 @@
-from fabric.api import run, cd, abort, require, sudo, env, put
+from fabric.api import run, cd, abort, require, sudo, env, put, path
 from fabric.decorators import runs_once, roles
 from fabric.contrib.console import confirm
 
@@ -9,7 +9,11 @@ def default():
   activate()
 
 def install():
-  sudo("yum install git redis nodejs npm --enablerepo=epel -y", warn_only=True)
+  sudo("yum install git redis --enablerepo=epel -y", warn_only=True)
+  sudo("wget http://nodejs.org/dist/v0.12.2/node-v0.12.2-linux-x64.tar.gz -O node-v0.12.2.tar.gz")
+  sudo("tar zxvf node-v0.12.2.tar.gz")
+  path("~/node-v0.12.2/bin")
+  # sudo("yum install git redis nodejs npm --enablerepo=epel -y", warn_only=True)
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
   sudo("yum --enablerepo=remi install redis -y", warn_only=True)
   sudo("npm i -g forever", warn_only=True)
