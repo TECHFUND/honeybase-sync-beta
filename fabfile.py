0d557006c993dba49f6171381b8cf9fdd3841228
@sgtn <shogo1104@gmail.com>
2015-07-26 07:52:00 +0900

    forever just not sudo

--- fabfile.py
+++ fabfile.py
@@ -9,9 +9,13 @@ def default():
   activate()
 
 def install():
-  sudo("yum install git redis --enablerepo=epel -y", warn_only=True)
+  # node
   sudo("wget http://nodejs.org/dist/v0.12.2/node-v0.12.2-linux-x64.tar.gz")
   sudo("tar zxvf node-v0.12.2-linux-x64.tar.gz")
+  sudo("ln -s ~/node-v0.12.2-linux-x64/bin/node /usr/bin/node")
+  sudo("ln -s ~/node-v0.12.2-linux-x64/bin/npm /usr/bin/npm")
+
+  sudo("yum install git redis --enablerepo=epel -y", warn_only=True)
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
   sudo("yum --enablerepo=remi install redis -y", warn_only=True)
   sudo("npm i -g forever", warn_only=True)
@@ -19,12 +23,10 @@ def install():
 def setup():
   put("./conf/redis.conf", "/etc/redis.hb.conf", use_sudo=True)
   sudo("git clone https://github.com/shogochiai/honeybase-sync-beta.git", warn_only=True);
-  sudo("ln -s ~/node-v0.12.2-linux-x64/bin/node /usr/bin/node")
-  sudo("ln -s ~/node-v0.12.2-linux-x64/bin/npm /usr/bin/npm")
 
 def activate():
   with cd("./honeybase-sync-beta"):
     # sudo("redis-server /etc/redis.hb.conf", warn_only=True)
     sudo("npm i", warn_only=True)
     sudo("redis-server /etc/redis.conf", warn_only=True)
-    sudo("forever start --killTree true --uid 'main' -a honeybase-sync.js", warn_only=True)
+    run("forever start --killTree true --uid 'main' -a honeybase-sync.js", warn_only=True)
