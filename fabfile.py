f4a0e889c90125d0dbc79bd548f2e345e8b39863
shogochiai <shogo1104@gmail.com>
2015-08-03 15:04:43 +0900

    unnessesally dependency deleted

--- fabfile.py
+++ fabfile.py
@@ -5,7 +5,6 @@ from fabric.contrib.console import confirm
 
 def default():
   install()
-  setup()
   activate()
 
 def install():
@@ -15,18 +14,14 @@ def install():
   sudo("ln -s ~/node-v0.12.2-linux-x64/bin/node /usr/bin/node")
   sudo("ln -s ~/node-v0.12.2-linux-x64/bin/npm /usr/bin/npm")
 
+  # git redis forever npm
   sudo("yum install git redis --enablerepo=epel -y", warn_only=True)
   sudo("rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm", warn_only=True)
   sudo("yum --enablerepo=remi install redis -y", warn_only=True)
   sudo("npm i -g forever", warn_only=True)
-
-def setup():
-  put("./conf/redis.conf", "/etc/redis.hb.conf", use_sudo=True)
   sudo("git clone https://github.com/shogochiai/honeybase-sync-beta.git", warn_only=True);
+  with cd("./honeybase-sync-beta"):
+    sudo("npm i", warn_only=True)
 
 def activate():
-  with cd("./honeybase-sync-beta"):
-    # sudo("redis-server /etc/redis.hb.conf", warn_only=True)
-    sudo("npm i", warn_only=True)
-    sudo("redis-server /etc/redis.conf", warn_only=True)
-    run("forever start --killTree true --uid 'main' -a honeybase-sync.js", warn_only=True)
+    sudo("redis-server /etc/redis.conf &", warn_only=True)
